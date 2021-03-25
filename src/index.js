216f61eb4b48d177bbf2e28b0dedb25e61008d92
chris <chris.mahfouz@hotmail.com>
2021-03-25 19:31:45 +0200

    completed assignment tasklister mini projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  let newForm=document.getElementById("create-task-form")
+  newForm.addEventListener("submit",addToDO)
+  let newList=document.getElementById("tasks")
+  newList.addEventListener('click',deleteToDo)
+})
+  
+
+
+function addToDO(event){
+  event.preventDefault()
+
+  let newDescription=document.getElementById("new-task-description")
+  let value=newDescription.value
+  let newList=document.getElementById("tasks")
+  let newLi=document.createElement('li')
+  newLi.innerText=value
+  newList.appendChild(newLi)
+
+
+  let button=document.createElement("button")
+  button.innerText="X"
+  newLi.append(button)
+  newDescription.value=''
+
+
+}
+
+
+function deleteToDo(event){
+  console.log(event)
+  event.target.parentElement.remove()
+
+
+
+}
